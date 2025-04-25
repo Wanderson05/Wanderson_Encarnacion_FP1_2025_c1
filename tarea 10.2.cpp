9a58d8493b78ca7afad7c79d9c54cbb967b8a410
Wanderson_Encarnación_FP1_2025_c1 <encarnacionmedinawanderson@gmail.com>
2025-04-25 19:39:51 -0400

    diez

---
+++ tarea 10.2.cpp
@@ -0,0 +1,32 @@
+Resultado A:
+| Nombre    | Puesto     | Salario |
+|-----------|------------|---------|
+| Fernando  | Obrero     | 3 200   |
+| Carlos    | Supervisor | 5 100   |
+
+
+
+
+
+
+Resultado B:
+| Código | Puesto    | Departamento |
+|--------|-----------|--------------|
+| 6 072  | Obrero    | Producción    |
+| 7 512  | Obrero    | Producción    |
+| 7 020  | Secretaria| Contabilidad |
+
+
+
+
+
+Resultado C:
+| Nombre    | Departamento   | Salario |
+|-----------|----------------|---------|
+| José      | Mantenimiento   | 4 300   |
+| Alicia    | R. Humanos      | 2 800   |
+| Carmen    | Contabilidad    | 3 000   |
+
+
+
+
